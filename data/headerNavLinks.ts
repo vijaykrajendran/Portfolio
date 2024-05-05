772214be6f0e64a2655bb7e0f8b90bece2e46c42
vijaykrajendran <vijaykrajendran69@gmail.com>
2024-05-05 19:08:16 +0200

    adding site details

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -5,6 +5,9 @@ const headerNavLinks = [
   // { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
   { href: '/contact', title: 'Contact' },
+  {href: '/testing', title: '[Honnesh](https://honneshraju.cloud)</i>'}
 ];
 
 export default headerNavLinks;
+
+
