97f6d7ba98bcefe7181f61febeb547f2c8b86c34
Chandangowdashankar <chandanshankar1999@gmail.com>
2026-09-25 02:35:02 +0100

    fix: resolve no-page-custom-font warning + fix flaky e2e assertion
    - Move Google Fonts (JetBrains Mono, Inter) from page Head to _document
    - Relax home e2e cert-pill assertion to match icon-prefixed label

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,16 @@ class MyDocument extends Document {
     return (
       <Html lang='en' className='scroll-smooth'>
         <Head>
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link
+            rel='preconnect'
+            href='https://fonts.gstatic.com'
+            crossOrigin='anonymous'
+          />
+          <link
+            href='https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;600;700&family=Inter:wght@400;500;600;700&display=swap'
+            rel='stylesheet'
+          />
           <link
             rel='apple-touch-icon'
             sizes='76x76'
